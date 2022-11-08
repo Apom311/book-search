925a9318f5cc19776c924218a10860def6009779
Apon wuz here <frost_bird@hotmail.com>
2022-11-08 12:09:00 -0600

    updated client-side component with mutations in mind

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -6,8 +6,8 @@ import {
   Card,
   Button,
 } from "react-bootstrap";
-
-import { getMe, deleteBook } from "../utils/API";
+// original code for reference
+// import { getMe, deleteBook } from "../utils/API";
 import Auth from "../utils/auth";
 import { removeBookId } from "../utils/localStorage";
 
